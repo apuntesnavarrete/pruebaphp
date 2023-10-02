753dc2b0a1f6bad443703a2a0d2d12d00d59bb6e
apuntesnavarrete <apuntesnavarrete@gmail.com>
2023-10-02 15:24:26 -0600

    initial file

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Mi primera página web PHP</title>
+</head>
+<body>
+  <h1>Hola mundo!</h1>
+  <?php
+    echo "Esta es mi primera página web PHP.";
+  ?>
+</body>
+</html>
